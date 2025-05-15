d0e82ca943e1909e9fba97e04162a58ce152ab9f
luismi <luisming96@gmail.com>
2025-05-15 10:36:25 +0200

    añadiendo css js y imagenes

---
+++ html_pisa4_A_nuno/js_pisa4_A_nuno/js_nuno.js
@@ -0,0 +1,4 @@
+/* Archivo JavaScript personalizado para el proyecto PISA4 - Nuno */
+document.addEventListener('DOMContentLoaded', function() {
+    // Código JavaScript personalizado puede ir aquí si es necesario
+});
